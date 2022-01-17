4df8a7d075fe592084a5012a89c1a3f4b4fd56d0
Mayowa Paul <lexypaul13@yahoo.com>
2022-01-17 16:45:22 -0500

    Fixed collection view height

--- RecipeGuide/Controllers/RecipeVC.swift
+++ RecipeGuide/Controllers/RecipeVC.swift
@@ -20,11 +20,10 @@ class RecipeVC: UIViewController {
         getRecipes()
         collectionView.delegate = self
         collectionView.dataSource =  self
-        
-        let layout = self.collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-        layout.sectionInset = UIEdgeInsets(top: 20, left: 5, bottom: 0, right: 5 )
-        layout.minimumLineSpacing = 5
-        layout.itemSize = CGSize(width:(self.collectionView.frame.size.width-20)/2, height: self.collectionView.frame.size.height/3)
+       let layout = self.collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+       layout.sectionInset = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5 )
+       layout.minimumLineSpacing = 40
+       layout.itemSize = CGSize(width:(self.collectionView.frame.size.width-20)/2, height: self.collectionView.frame.size.height/3)
     }
     
     func getRecipes(){
@@ -44,29 +43,31 @@ class RecipeVC: UIViewController {
     
 }
 
-extension RecipeVC: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
-    
+
+
+
+extension RecipeVC:UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return category.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "foodCell", for: indexPath) as! RecipeCollectionViewCell
         cell.setupCell(categories: category.sorted(by: { category_1, category_2 in
             category_1.strCategory < category_2.strCategory
         })[indexPath.row])
         return cell
     }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width/2, height: collectionView.frame.width / 2)
+    }
     
-
+   
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let vc = storyboard?.instantiateViewController(identifier: "MealsVC") as? MealsVC
         vc?.meal = category[indexPath.row].strCategory
         self.navigationController?.pushViewController(vc!, animated: true)
-        
     }
-    
-    
 }
-
